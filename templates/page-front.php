8ca4a98da9fa917a9b02424fc047087bf9614414
Markus Hedenborn <markushedenborn@gmail.com>
2019-09-12 08:04:19 +0200

    weather widget css

--- templates/page-front.php
+++ templates/page-front.php
@@ -33,17 +33,17 @@ $cover = get_field('cover-img');
           </div>
         </div>
         <div class="col-md-6 col-lg-5">
-          <div class="media current-weather" data-aos="fade-left" data-aos-delay="200">
+          <div class="media current-weather aos-animate aos-animate-custom " data-aos="fade-left" data-aos-delay="200">
             <img class="align-self-center" src="http://openweathermap.org/img/wn/<?php echo $icon; ?>@2x.png">
             <div class="media-body">
               <ul class="current-weather-data">
-                <li class="weather-desc"><span class="font-main"><?php echo $maind; ?></span><span class="font-head"><?php echo number_format($temp, 1); ?>&deg;</span></li>
-                <li class="minmax"><span class="text">min</span><span class="font-head"><?php echo number_format($mintemp, 1); ?>&deg;</span></li>
-                <li class="minmax"><span class="text">max</span><span class="font-head"><?php echo number_format($maxtemp, 1); ?>&deg;</span></li>
-                <li class="wind"><span>wind</span><span class="font-head"><?php echo $wind; ?> m/s</span><img class="icon-wind" src="<?php echo get_template_directory_uri() ?>/assets/images/wind-white.svg" alt="wind icon"></li>
+                <li class="weather-desc"><span class="font-main"><?php echo $maind; ?></span><span class="font-head last"><?php echo number_format($temp, 1); ?>&deg;</span></li>
+                <li class="minmax"><span>min</span><span class="font-head last"><?php echo number_format($mintemp, 1); ?>&deg;</span></li>
+                <li class="minmax"><span>max</span><span class="font-head last"><?php echo number_format($maxtemp, 1); ?>&deg;</span></li>
+                <li class="wind"><span>vind</span><span class="font-head"><?php echo $wind; ?> m/s</span><img class="icon-wind" src="<?php echo get_template_directory_uri() ?>/assets/images/wind-white.svg" alt="wind icon"></li>
               </ul>
             </div>
-            <span class="label">Vädret just nu.</span>
+            <span class="label">Vädret i Coveta Fumá just nu</span>
           </div>
         </div>
       </div>
