995d8fd4c536e993537d3d623e2545c7192b1d80
Markus Hedenborn <markushedenborn@gmail.com>
2019-09-12 21:05:16 +0200

    archive singel

--- single-stories.php
+++ single-stories.php
@@ -6,24 +6,50 @@
  */
 ?>
 
-<div class="page-container">
+<div class="page-container post-singel">
   <?php if (have_posts()) : ?>
-    <div class="page-wrapper">
-      <?php while (have_posts()) : the_post(); ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <?php
+          $main_thumb = get_field('post-main-img');
+          $special_thumb = get_field('extra-img');
+          $post_ingress = get_field('post-ingress');
+          $post_content = get_field('post-text');
 
+          $hero = ($special_thumb) ? $special_thumb : $main_thumb;
+          ?>
 
-        <div class="container">
+      <div class="container">
 
-          <div class="row">
-            <div class="col col-md-12 d-flex align-items-center">
-              <h1 class="divider-title"><?php the_title(); ?></h1>
+        <div class="row">
+          <div class="col col-12">
+            <div class="post-singel-hero" style="background-image:url(<?php echo $hero['url']; ?>)"></div>
+          </div>
+        </div>
+
+        <div class="row row-eq-height justify-content-center">
+          <div class="col col-10 text-center post-singel">
+            <time datetime="<?php echo get_the_date(); ?>">
+              <?php echo get_the_date(); ?>
+            </time>
+            <h1 class="post-singel-title"><?php the_title(); ?></h1>
+            <?php if ($post_ingress) : ?>
+              <div class="post-singel-ingress">
+                <p class="">
+                  <?php echo $post_ingress; ?>
+                </p>
+                <div class="post-singel-ingress-divider"></div>
+              </div>
+            <?php endif; ?>
+          </div>
+          <div class="col-md-8 justify-content-center">
+            <div class="post-singel-content">
+              <?php echo $post_content; ?>
             </div>
           </div>
-
-
-
-
-        <?php endwhile; ?>
         </div>
-      <?php endif; ?>
-    </div>
+
+      </div>
+
+    <?php endwhile; ?>
+  <?php endif; ?>
+</div>
